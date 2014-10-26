f3bfd63fd5fd8ce3c3cf30d7b8afc0759c6de12c
Joshua Augustin <augustinspring@gmail.com>
2014-10-26 17:42:22 -0400

    Added hex player.

--- src/watermelon/group7/Player.java
+++ src/watermelon/group7/Player.java
@@ -7,7 +7,7 @@ import watermelon.sim.Point;
 import watermelon.sim.seed;
 
 public class Player extends watermelon.sim.Player {
-	static double distowall = 2.1;
+	static double distowall = 1.1;
 	static double distotree = 2.2;
 	static double distoseed = 1.01;
 
@@ -15,11 +15,20 @@ public class Player extends watermelon.sim.Player {
 
 	}
 
+    public double getHexXOffset() {
+        return 2 * distoseed / Math.cos(Math.PI / 4.0);
+    }
+
+    public double getHexYOffset() {
+        return 2 * distoseed / Math.sin(Math.PI / 4.0);
+    }
+
 	@Override
 	public ArrayList<seed> move(ArrayList<Pair> treelist, double width, double length, double s) {
 		ArrayList<seed> seedlist = new ArrayList<seed>();
+        boolean shift = false;
 		for (double i = distowall; i < width - distowall; i = i + distoseed) {
-			for (double j = distowall; j < length - distowall; j = j + distoseed) {
+			for (double j = distowall; j < length - distowall; j = j + getHexYOffset()) {
 				Random random = new Random();
 
 				seed tmp;
@@ -27,6 +36,10 @@ public class Player extends watermelon.sim.Player {
 					tmp = new seed(i, j, false);
 				else
 					tmp = new seed(i, j, true);
+
+                if (shift) {
+                    tmp.i += getHexXOffset();
+                }
 
 				boolean add = true;
 				for (int f = 0; f < treelist.size(); f++) {
@@ -40,6 +53,8 @@ public class Player extends watermelon.sim.Player {
 					seedlist.add(tmp);
 				}
 			}
+
+            shift = !shift;
 		}
 
 		System.out.printf("seedlist size is %d", seedlist.size());
