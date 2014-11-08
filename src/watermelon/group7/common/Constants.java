3a6d2f340817583251c55b4d02e2a87434ddc29d
Joshua Augustin <augustinspring@gmail.com>
2014-11-08 16:02:35 -0500

    Added stuff to Analysis and Constants.

--- src/watermelon/group7/common/Constants.java
+++ src/watermelon/group7/common/Constants.java
@@ -1,7 +1,11 @@
 package watermelon.group7.common;
 
 public class Constants {
-	public static final double wall_spacing = 0.999999;
+	public static final double wall_spacing = 1.;
 	public static final double tree_diameter = 2.;
 	public static final double seed_diameter = 2.;
+
+    public static final double PI_OVER_6 = 0.52359877559;
+    public static final double TWO_PI = 6.28318530718;
+    public static final double PI_OVER_3 = 1.0471975512;
 }
