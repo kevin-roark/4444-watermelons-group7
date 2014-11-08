50febe8af7abd7eb7a9bf4da8dabf1826dcf48de
Joshua Augustin <augustinspring@gmail.com>
2014-11-08 15:30:44 -0500

    Added Vector2.java.

---
+++ src/watermelon/group7/common/Vector2.java
@@ -0,0 +1,87 @@
+package watermelon.group7.common;
+
+import watermelon.sim.Pair;
+import watermelon.sim.seed;
+
+public class Vector2 {
+    public double x;
+    public double y;
+
+    public Vector2() {
+        this(0,0);
+    }
+
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2(Pair p) {
+        this(p.x, p.y);
+    }
+    public Vector2(seed s) {
+        this(s.x, s.y);
+    }
+
+    public Vector2 add(Vector2 r) {
+        return new Vector2(x+r.x, y+r.y);
+    }
+
+    public Vector2 add(double dx, double dy) {
+        return new Vector2(x+dx, y+dy);
+    }
+
+    public Vector2 sub(Vector2 r) {
+        return new Vector2(x-r.x, y-r.y);
+    }
+    public Vector2 sub(double dx, double dy) {
+        return new Vector2(x-dx, y-dy);
+    }
+
+    public Vector2 scale(double s) {
+        return new Vector2(this.x * s, this.y * s);
+    }
+
+    public Vector2 unit() {
+        return this.scale(1.0 / this.length());
+    }
+
+    public double angle() {
+        return Math.atan2(y, x);
+    }
+
+    public Vector2 rotate(double rads) {
+        double c = Math.cos(rads);
+        double s = Math.sin(rads);
+
+        return new Vector2(c*x - s*y, 
+                           s*x + c*y);
+    }
+
+    public double length() {
+        return Math.sqrt(x*x + y*y);
+    }
+
+    public double dot(Vector2 other) {
+        return this.x * other.x + this.y * other.y;
+    }
+
+    public Vector2 rotateRelative(Vector2 origin, double theta) {
+        Vector2 adjusted = this.sub(origin);
+        return adjusted.rotate(theta);
+    }
+
+    public static Vector2 fromOrientationAndLength(double theta, double r) {
+        return new Vector2(r * Math.cos(theta), 
+                           r * Math.sin(theta));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("<Vector2 (%f, %f)>", x, y);
+    }
+
+    public static double dot(Vector2 l, Vector2 r) {
+        return l.dot(r);
+    }
+}
